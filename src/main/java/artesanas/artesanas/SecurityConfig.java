e072ccfb5481ad6f31c375e51d73b729eb168a35
Ana Cristina Martinez <am8794041@gmail.com>
2024-12-03 22:12:01 -0600

    seguridad

--- src/main/java/artesanas/artesanas/SecurityConfig.java
+++ src/main/java/artesanas/artesanas/SecurityConfig.java
@@ -14,7 +14,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-    /* @Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
        http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
@@ -26,32 +26,7 @@ public class SecurityConfig {
                 .rememberMe(withDefaults())
                 .logout(logout -> logout.logoutUrl("/signout").permitAll());
         return http.build();
-    } */
-
-    @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable) // Deshabilitar CSRF si no es necesario
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/signin", "/signup").permitAll() // Permitir acceso sin autenticación
-            .requestMatchers("/customers/**", "/payments/**", "/shoppings/**", "/carts/**", "/shippings/**", "/addresses/**", "/productsCarts/**").hasRole("ADMIN")
-            .requestMatchers("/doc/swagger-ui/**").authenticated() // Requerir autenticación para Swagger
-            .requestMatchers("/").authenticated() // Requerir autenticación para la raíz
-            .anyRequest().authenticated() // Todas las demás rutas requieren autenticación
-        )
-        .formLogin(form -> form
-            .loginPage("/signin") // Página de inicio de sesión personalizada
-            .defaultSuccessUrl("/", true) // Redirigir a la raíz después de autenticarse
-        )
-        .logout(logout -> logout
-            .logoutUrl("/signout") // URL para cerrar sesión
-            .logoutSuccessUrl("/signin") // Redirigir a la página de inicio de sesión tras cerrar sesión
-            .permitAll()
-        )
-        .rememberMe(withDefaults()); // Configuración de "recordar usuario"
-
-    return http.build();
-}
-
+    }
     
 
     @Bean
